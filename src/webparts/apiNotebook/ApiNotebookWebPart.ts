d4de480bbc424436459e2726923a7c0a3215b8ae
Martin Amm <ma@adenin.com>
2021-04-23 15:22:37 +0100

    request and forward msgraph token

--- src/webparts/apiNotebook/ApiNotebookWebPart.ts
+++ src/webparts/apiNotebook/ApiNotebookWebPart.ts
@@ -4,6 +4,7 @@ import {
   PropertyPaneTextField
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { AadTokenProvider } from '@microsoft/sp-http';
 import { escape } from '@microsoft/sp-lodash-subset';
 
 import styles from './ApiNotebookWebPart.module.scss';
@@ -18,45 +19,68 @@ export interface IApiNotebookWebPartProps {
 
 export default class ApiNotebookWebPart extends BaseClientSideWebPart<IApiNotebookWebPartProps> {
 
+
   public render(): void {
 
-    console.log("render " + (this.displayMode == DisplayMode.Edit ? "edit" : "view"));
+    this.context.statusRenderer.displayLoadingIndicator(this.domElement, '');
 
-    var sbId = "api-sandbox-" + this.instanceId.split("-").join("");
+    this.context.aadTokenProviderFactory
+      .getTokenProvider()
+      .then((tokenProvider: AadTokenProvider): Promise<string> => {
+        debugger;
+        // retrieve access token for the enterprise API secured with Azure AD
+        // the parameter passed into getToken()is the Application ID URI
+        return tokenProvider.getToken('https://graph.microsoft.com');
+      })
+      .then((accessToken: string): void => {
 
-    var sb = document.getElementById(sbId);
-    if (sb) sb.classList.toggle("hidden", true);
-    if (!sb) {
-      sb = new StarboardNotebookIFrame({
-        notebookContainer: this,
-        notebookContent: this.properties.notebookContent,
-        notebookVariables: {},
-        debug: false,
-        notebookEditMode: this.displayMode == DisplayMode.Edit ? "edit" : "view",
-        src: "https://apinotebooks-sandbox.netlify.app",
-        onContentUpdateMessage: function (evt) {
-          this.notebookContainer.properties.notebookContent = evt.content;
+        this.context.statusRenderer.clearLoadingIndicator(this.domElement);
+
+        console.log("render " + (this.displayMode == DisplayMode.Edit ? "edit" : "view"));
+
+        var sbId = "api-sandbox-" + this.instanceId.split("-").join("");
+
+        var sb = document.getElementById(sbId);
+        if (sb) sb.classList.toggle("hidden", true);
+
+        var editMode:string =  this.displayMode == DisplayMode.Edit ? "edit" : "view";
+        var variables:any = {};
+        variables._token = "office-365:" + accessToken;
+
+        if (!sb) {
+          sb = new StarboardNotebookIFrame({
+            notebookContainer: this,
+            notebookContent: this.properties.notebookContent,
+            notebookVariables: variables,
+            debug: false,
+            notebookEditMode: editMode,
+            src: "https://apinotebooks-sandbox.netlify.app",
+            onContentUpdateMessage: function (evt) {
+              this.notebookContainer.properties.notebookContent = evt.content;
+            }
+          });
+
+          sb.style.width = "100%";
+          sb.id = sbId;
+          this.domElement.innerHTML = "";
+          this.domElement.appendChild(sb);
+        } else {
+          // reload instance with new content  
+          // @ts-ignore    
+          sb.notebookContent = this.properties.notebookContent;
+          // @ts-ignore
+          sb.notebookVariables = variables;
+          // @ts-ignore    
+          sb.notebookEditMode = editMode;
+          // @ts-ignore
+          sb.sendMessage({
+            type: "NOTEBOOK_RELOAD_PAGE"
+          });
+          sb.classList.toggle("hidden", false);
         }
+
       });
 
-      sb.style.width = "100%";
-      sb.id = sbId;
-      this.domElement.innerHTML = "";
-      this.domElement.appendChild(sb);
-    } else {
-      // reload instance with new content  
-      // @ts-ignore    
-      sb.notebookContent = this.properties.notebookContent;
-      // @ts-ignore
-      sb.notebookVariables = {};    
-      // @ts-ignore    
-      sb.notebookEditMode = this.displayMode == DisplayMode.Edit ? "edit" : "view";
-      // @ts-ignore
-      sb.sendMessage({
-        type: "NOTEBOOK_RELOAD_PAGE"
-      });
-      sb.classList.toggle("hidden", false);
-    }
 
     return;
   }
